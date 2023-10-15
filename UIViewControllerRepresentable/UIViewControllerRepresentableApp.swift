314d8540db369050d9f976cd5793875fb97963ef
nindyalita <118523607+nindyalita@users.noreply.github.com>
2023-10-15 21:27:34 +0700

    Initial Commit

---
+++ UIViewControllerRepresentable/UIViewControllerRepresentableApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIViewControllerRepresentableApp.swift
+//  UIViewControllerRepresentable
+//
+//  Created by Nindya Alita Rosalia on 15/10/23.
+//
+
+import SwiftUI
+
+@main
+struct UIViewControllerRepresentableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
